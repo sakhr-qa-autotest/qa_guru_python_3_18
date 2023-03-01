4f5b4d0f3ff304385b5c02c07ec83848b040e47b
sakhr-qa-autotest <salavat.kharasov@redlab.dev>
2023-03-01 16:00:22 +0400

    Поток 3 домашняя работа 19

--- tests/regres/test_api.py
+++ tests/regres/test_api.py
@@ -1,3 +1,4 @@
+import allure
 from pytest_voluptuous import S
 
 from schemas.user import user
@@ -12,37 +13,42 @@ testUser = {
 
 
 def test_status_code():
-    response = regres.get('users?page=1')
+    with allure.step('Check status code'):
+        response = regres.get('users?page=1')
 
-    assert response.status_code == 200
+        assert response.status_code == 200
 
 
 def test_schema():
-    response = regres.get('users/2')
-    assert S(user) == response.json()['data']
+    with allure.step('Schema'):
+        response = regres.get('users/2')
+        assert S(user) == response.json()['data']
 
 
 def test_login_successful():
-    response = regres.post('login', json={
-        "email": "eve.holt@reqres.in",
-        "password": "cityslicka"
-    })
+    with allure.step('Login successful'):
+        response = regres.post('login', json={
+            "email": "eve.holt@reqres.in",
+            "password": "cityslicka"
+        })
 
     assert response.status_code == 200
     assert len(response.json().get('token')) >= 1
 
 
 def test_login_unsuccessful():
-    response = regres.post('login', json={
-        "email": "eve.holt@reqres.in"
-    })
+    with allure.step('Login unsuccessful'):
+        response = regres.post('login', json={
+            "email": "eve.holt@reqres.in"
+        })
 
-    assert response.status_code == 400
-    assert response.json().get('error') >= 'Missing password'
+        assert response.status_code == 400
+        assert response.json().get('error') >= 'Missing password'
 
 
 def test_create_successful():
-    response = regres.post('users', json=testUser)
+    with allure.step('Create successful'):
+        response = regres.post('users', json=testUser)
 
-    assert response.status_code == 201
-    assert response.reason == 'Created'
+        assert response.status_code == 201
+        assert response.reason == 'Created'
