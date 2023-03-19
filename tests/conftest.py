fef64c31af19ded6682409db0e3a6b1324942d3f
sakhr-qa-autotest <salavat.kharasov@redlab.dev>
2023-03-19 23:53:18 +0400

    Фикс

--- tests/conftest.py
+++ tests/conftest.py
@@ -14,12 +14,21 @@ def env(request):
 
 
 @pytest.fixture(scope='session')
-def window(demoqa):
+def cookie(demoqa):
+    result = demoqa.login('testuser123@mail.com', 'testuser123')
+    demoqa.authorization_cookie(result)
+    return demoqa.authorizationCookie
+
+
+@pytest.fixture(scope='function')
+def window(demoqa, cookie):
     browser.config.base_url = demoqa.demoqa.url
     browser.config.window_width = 1920
     browser.config.window_height = 1080
     browser.open(demoqa.demoqa.url)
-    return browser
+    browser.driver.add_cookie(cookie)
+    yield browser
+    browser.quit()
 
 
 @pytest.fixture(scope='session')
--- tests/demoshop/test_ui.py
+++ tests/demoshop/test_ui.py
@@ -2,19 +2,15 @@ import allure
 from selene import have
 
 
-def test_register(window, demoqa):
-    result = demoqa.login('sopemoc825@vootin.com', '4!sG!enS!Kieg')
-    demoqa.authorization_cookie(result)
-    window.driver.add_cookie(demoqa.authorizationCookie)
-
-
 def test_open_account(window):
+    window.open("")
     with allure.step('Open an account'):
         window.element('a.account').click()
         window.element('div.page-title > h1').should(have.text('My account - Customer info'))
 
 
 def test_clear_cart(window, demoqa):
+    window.open("")
     with allure.step('Clear cart'):
         demoqa.demoqa.post('/addproducttocart/catalog/31/1/1')
         window.element('.ico-cart').click()
@@ -25,12 +21,14 @@ def test_clear_cart(window, demoqa):
 
 
 def test_successful_search(window):
+    window.open("")
     with allure.step('Successful search'):
         window.element('.search-box-text').type('Нокиа 3310').press_enter()
         window.element('.result').should(have.text('No products were found that matched your criteria.'))
 
 
 def test_logout(window, demoqa):
+    window.open("")
     with allure.step('Logout'):
         window.element('.ico-logout').click()
         response = demoqa.demoqa.get('/logout', allow_redirects=False)
